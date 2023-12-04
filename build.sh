fed0ce5edbea98af60228b737736b484ae449bc9
= <alfred@stensatter.se>
2023-12-05 00:14:03 +0100

    added man page

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 gcc -shared -fPIC -o ./build/libarraylist.so ./src/arraylist.c
 cp ./src/arraylist.h ./build
+cp ./src/arraylist.man ./build
 
 gcc -o ./build/arraylist.o -c ./src/arraylist.c
 ar rcs ./build/libarraylist.a ./build/libarraylist.so
